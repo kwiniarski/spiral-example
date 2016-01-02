f6a26107b9c9a4efe059295b97e26d458b42b918
Krzysztof Winiarski <winiarski.krzysztof@gmail.com>
2016-01-02 15:15:01 +0100

    fix Semaphore MySQL credentials

--- config/models.js
+++ config/models.js
@@ -6,11 +6,13 @@
 
 'use strict';
 
+var E = process.env;
+
 module.exports = {
   connection: {
     database: 'test_spiral',
-    user: process.env.DATABASE_MYSQL_USERNAME || process.env.USER,
-    password: process.env.DATABASE_MYSQL_PASSWORD || ''
+    user: E.DATABASE_MYSQL_USERNAME || E.USER,
+    password: E.DATABASE_MYSQL_PASSWORD || ''
   },
   options: {
     dialect: 'mysql',
